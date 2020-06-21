495c93362179ac0929c4802d167f76f5f2878c9b
borisvidosevic <boris.vidosevic@verybigthings.com>
2020-06-21 13:51:12 +0200

    init

---
+++ dragme_ui/src/components/Main.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect, useRef } from "react";
+
+let swipeDeltaX = 0;
+let swipeDeltaY = 0;
+let requiredSwipeDelta = 73;
+
+function Main(){
+
+	return (
+
+		<div className="main">
+			<div>
+				test
+			</div>
+		</div>
+
+	);
+}
+
+export default Main;
